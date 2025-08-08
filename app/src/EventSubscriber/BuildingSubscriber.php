08ff280a8ad9768c0c6ecb0648ba504da73c43ca
Cédric Moclyn <cedric.moclyn@hotmail.fr>
2025-08-08 08:58:55 +0200

    feat(building): add planetBuilding on building creation

--- app/src/EventSubscriber/BuildingSubscriber.php
+++ app/src/EventSubscriber/BuildingSubscriber.php
@@ -3,17 +3,25 @@
 namespace App\EventSubscriber;
 
 use App\Entity\Building;
+use App\Entity\PlanetBuildings;
+use App\Repository\PlanetRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 readonly class BuildingSubscriber implements EventSubscriberInterface
 {
-    public function __construct() {}
+    public function __construct(
+        private PlanetRepository $planetRepository,
+        private EntityManagerInterface $entityManager,
+    ) {}
 
     public static function getSubscribedEvents(): array
     {
         return [
             BeforeEntityPersistedEvent::class => 'beforeBuildingCreated',
+            AfterEntityPersistedEvent::class => 'afterBuildingCreated',
         ];
     }
 
@@ -27,5 +35,24 @@ readonly class BuildingSubscriber implements EventSubscriberInterface
         foreach ($building->getBasePrices() as $basePrice) {
             $basePrice->setBuilding($building);
         }
+    }
+
+    public function afterBuildingCreated(AfterEntityPersistedEvent $event): void
+    {
+        $building = $event->getEntityInstance();
+        if (!($building instanceof Building)) {
+            return;
+        }
+
+        $planets = $this->planetRepository->getPlanetsWithOwner();
+        foreach ($planets as $planet) {
+            $planetBuilding = new PlanetBuildings();
+            $planetBuilding->setBuilding($building);
+            $planetBuilding->setPlanet($planet);
+            $planetBuilding->setLevel(0);
+            $planet->addBuilding($planetBuilding);
+            $this->entityManager->persist($planetBuilding);
+        }
+        $this->entityManager->flush();
     }
 }
